a5277c5313e51e62bba0d9c5024d83035d41bf28
christianekstrand <christianekstrand@gmail.com>
2014-09-04 16:22:33 +0200

    Basic implementation for fetching contact information from an Ad.

---
+++ src/main/java/com/youcruit/ams/api/client/object/Ad.java
@@ -0,0 +1,62 @@
+package com.youcruit.ams.api.client.object;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Ad {
+    @JacksonXmlProperty(localName="ansokan")
+    private Application application;
+    @JacksonXmlProperty(localName="arbetsplats")
+    private Workplace workplace;
+    public Application getApplication() {
+        return application;
+    }
+    public void setApplication(Application application) {
+        this.application = application;
+    }
+    public Workplace getWorkplace() {
+        return workplace;
+    }
+    public void setWorkplace(Workplace workplace) {
+        this.workplace = workplace;
+    }
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((application == null) ? 0 : application.hashCode());
+	result = prime * result + ((workplace == null) ? 0 : workplace.hashCode());
+	return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj) return true;
+	if (obj == null) return false;
+	if (!(obj instanceof Ad)) return false;
+	Ad other = (Ad) obj;
+	if (application == null) {
+	    if (other.application != null) return false;
+	} else if (!application.equals(other.application)) return false;
+	if (workplace == null) {
+	    if (other.workplace != null) return false;
+	} else if (!workplace.equals(other.workplace)) return false;
+	return true;
+    }
+    @Override
+    public String toString() {
+	return "Ad [application=" + application + ", workplace=" + workplace + "]";
+    }
+    public String getEmail() {
+	String email = null;
+	if(getApplication()!=null && getApplication().getEmail() != null && getApplication().getEmail().length()>0){
+	    email = getApplication().getEmail();
+	}
+	if((email==null || email.equals("")) && workplace!=null){
+	    if(getWorkplace().getEmail()!=null && getWorkplace().getEmail().length() > 0){
+		email = getWorkplace().getEmail();
+	    }
+	}
+	return email;
+    }
+}
---
+++ src/test/java/com/youcruit/ams/api/client/object/AdTest.java
@@ -0,0 +1,122 @@
+package com.youcruit.ams.api.client.object;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class AdTest {
+
+    private InputStream is;
+    private ObjectMapper om;
+    private Ad expected;
+
+    @Before
+    public void setUp() {
+	is = Thread.currentThread().getContextClassLoader().getResourceAsStream("ad.xml");
+	om = new XmlMapper();
+	expected = new Ad();
+	Application application = new Application();
+	application.setEmail("hasanin.farhan@swedengreentrading.se");
+	expected.setApplication(application);
+	Workplace workplace = new Workplace();
+	workplace.setEmail("hasanin.farhan@swedengreentrading.se");
+	expected.setWorkplace(workplace);
+    }
+
+    @Test
+    public void testParseAd() throws JsonParseException, JsonMappingException, IOException {
+	Ad actual = om.readValue(is, Ad.class);
+	assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testGetEmailFromApplication(){
+	expected.setWorkplace(null);
+	String email = "111111@22222.3333";
+	expected.getApplication().setEmail(email);
+	assertEquals(email, expected.getEmail());
+    }
+    
+    @Test
+    public void testGetEmailFromApplicationWorkPlaceNoEmail(){
+	expected.getWorkplace().setEmail(null);
+	String email = "111111@22222.3333";
+	expected.getApplication().setEmail(email);
+	assertEquals(email, expected.getEmail());
+    }
+    
+    @Test
+    public void testGetEmailFromApplicationWorkPlaceEmnptyEmail(){
+	expected.getWorkplace().setEmail("");
+	String email = "111111@22222.3333";
+	expected.getApplication().setEmail(email);
+	assertEquals(email, expected.getEmail());
+    }
+    
+    @Test
+    public void testGetEmailFromApplicationEmptyString(){
+	expected.setWorkplace(null);
+	expected.getApplication().setEmail("");
+	assertNull(expected.getEmail());
+    }
+    
+    @Test
+    public void testGetEmailFromWorkplaceEmptyString(){
+	expected.getWorkplace().setEmail("");
+	expected.getApplication().setEmail("");
+	assertNull(expected.getEmail());
+    }
+    
+    @Test
+    public void testGetEmailFromWorkplaceEmptyStringApplicationNull(){
+	expected.getWorkplace().setEmail("");
+	expected.setApplication(null);
+	assertNull(expected.getEmail());
+    }
+    
+    @Test
+    public void testGetEmailFromApplicationNoApplication(){
+	expected.setWorkplace(null);
+	expected.setApplication(null);
+	assertNull(expected.getEmail());
+    }
+    @Test
+    public void testGetEmailFromWorkplace(){
+	expected.setApplication(null);
+	String email = "111111@22222.3333";
+	expected.getWorkplace().setEmail(email);
+	assertEquals(email, expected.getEmail());
+    }
+    
+    @Test
+    public void testGetEmailFromWorkplaceApplicationNoEmail(){
+	expected.getApplication().setEmail(null);
+	String email = "111111@22222.3333";
+	expected.getWorkplace().setEmail(email);
+	assertEquals(email, expected.getEmail());
+    }
+    
+    @Test
+    public void testGetEmailFromWorkplaceApplicationEmptyEmail(){
+	expected.getApplication().setEmail("");
+	String email = "111111@22222.3333";
+	expected.getWorkplace().setEmail(email);
+	assertEquals(email, expected.getEmail());
+    }
+    
+    @After
+    public void tearDown() throws IOException {
+	is.close();
+    }
+}
