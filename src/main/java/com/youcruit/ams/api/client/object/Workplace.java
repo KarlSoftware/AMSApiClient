867bbfa36f2f0972a79adb5669189a675fe1080d
christianekstrand <christianekstrand@gmail.com>
2014-09-04 16:46:12 +0200

    extended workplace to support more fields

--- src/main/java/com/youcruit/ams/api/client/object/Workplace.java
+++ src/main/java/com/youcruit/ams/api/client/object/Workplace.java
@@ -5,6 +5,28 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Workplace {
+    @JacksonXmlProperty(localName="arbetsplatsnamn")
+    private String name;
+    @JacksonXmlProperty(localName="postnummer")
+    private String postalCode;
+    @JacksonXmlProperty(localName="postadress")
+    private String adress;
+    @JacksonXmlProperty(localName="besoksadress")
+    private String visitingAddress;
+    @JacksonXmlProperty(localName="besoksort")
+    private String visitingCity;
+    @JacksonXmlProperty(localName="land")
+    private String visitingCountry;
+    @JacksonXmlProperty(localName="postort")
+    private String city;
+    @JacksonXmlProperty(localName="postland")
+    private String country;
+    @JacksonXmlProperty(localName="telefonnummer")
+    private String phone;
+    @JacksonXmlProperty(localName="faxnummer")
+    private String fax;
+    @JacksonXmlProperty(localName="hemsida")
+    private String homepage;
     @JacksonXmlProperty(localName="epostadress")
     private String email;
 
@@ -16,11 +38,110 @@ public class Workplace {
         this.email = email;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getVisitingAddress() {
+        return visitingAddress;
+    }
+
+    public void setVisitingAddress(String visitingAddress) {
+        this.visitingAddress = visitingAddress;
+    }
+
+    public String getVisitingCity() {
+        return visitingCity;
+    }
+
+    public void setVisitingCity(String visitingCity) {
+        this.visitingCity = visitingCity;
+    }
+
+    public String getVisitingCountry() {
+        return visitingCountry;
+    }
+
+    public void setVisitingCountry(String visitingCountry) {
+        this.visitingCountry = visitingCountry;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
     @Override
     public int hashCode() {
 	final int prime = 31;
 	int result = 1;
+	result = prime * result + ((adress == null) ? 0 : adress.hashCode());
+	result = prime * result + ((city == null) ? 0 : city.hashCode());
+	result = prime * result + ((country == null) ? 0 : country.hashCode());
 	result = prime * result + ((email == null) ? 0 : email.hashCode());
+	result = prime * result + ((fax == null) ? 0 : fax.hashCode());
+	result = prime * result + ((homepage == null) ? 0 : homepage.hashCode());
+	result = prime * result + ((name == null) ? 0 : name.hashCode());
+	result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+	result = prime * result + ((postalCode == null) ? 0 : postalCode.hashCode());
+	result = prime * result + ((visitingAddress == null) ? 0 : visitingAddress.hashCode());
+	result = prime * result + ((visitingCity == null) ? 0 : visitingCity.hashCode());
+	result = prime * result + ((visitingCountry == null) ? 0 : visitingCountry.hashCode());
 	return result;
     }
 
@@ -30,14 +151,48 @@ public class Workplace {
 	if (obj == null) return false;
 	if (!(obj instanceof Workplace)) return false;
 	Workplace other = (Workplace) obj;
+	if (adress == null) {
+	    if (other.adress != null) return false;
+	} else if (!adress.equals(other.adress)) return false;
+	if (city == null) {
+	    if (other.city != null) return false;
+	} else if (!city.equals(other.city)) return false;
+	if (country == null) {
+	    if (other.country != null) return false;
+	} else if (!country.equals(other.country)) return false;
 	if (email == null) {
 	    if (other.email != null) return false;
 	} else if (!email.equals(other.email)) return false;
+	if (fax == null) {
+	    if (other.fax != null) return false;
+	} else if (!fax.equals(other.fax)) return false;
+	if (homepage == null) {
+	    if (other.homepage != null) return false;
+	} else if (!homepage.equals(other.homepage)) return false;
+	if (name == null) {
+	    if (other.name != null) return false;
+	} else if (!name.equals(other.name)) return false;
+	if (phone == null) {
+	    if (other.phone != null) return false;
+	} else if (!phone.equals(other.phone)) return false;
+	if (postalCode == null) {
+	    if (other.postalCode != null) return false;
+	} else if (!postalCode.equals(other.postalCode)) return false;
+	if (visitingAddress == null) {
+	    if (other.visitingAddress != null) return false;
+	} else if (!visitingAddress.equals(other.visitingAddress)) return false;
+	if (visitingCity == null) {
+	    if (other.visitingCity != null) return false;
+	} else if (!visitingCity.equals(other.visitingCity)) return false;
+	if (visitingCountry == null) {
+	    if (other.visitingCountry != null) return false;
+	} else if (!visitingCountry.equals(other.visitingCountry)) return false;
 	return true;
     }
 
     @Override
     public String toString() {
-	return "Workplace [email=" + email + "]";
+	return "Workplace [name=" + name + ", postalCode=" + postalCode + ", adress=" + adress + ", visitingAddress=" + visitingAddress + ", visitingCity=" + visitingCity + ", visitingCountry=" + visitingCountry + ", city=" + city
+		+ ", country=" + country + ", phone=" + phone + ", fax=" + fax + ", homepage=" + homepage + ", email=" + email + "]";
     }
 }
---
+++ src/test/java/com/youcruit/ams/api/client/object/WorkplaceTest.java
@@ -0,0 +1,62 @@
+package com.youcruit.ams.api.client.object;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class WorkplaceTest {
+    /*<arbetsplatsnamn>SWEDEN GREEN TRADING AB</arbetsplatsnamn>
+	<postnummer>13140</postnummer>
+	<postadress>Kantatvägen 18</postadress>
+	<postort>Nacka</postort>
+	<postland>Sverige</postland>
+	<land>Sverige</land>
+	<besoksadress>Kantatvägen 18</besoksadress>
+	<besoksort>Nacka</besoksort>
+	<telefonnummer>076-2899405</telefonnummer>
+	<faxnummer />
+	<epostadress>11111@3333.3333</epostadress>
+	<hemsida>http://www.swedengreentrading.se</hemsida>*/
+    private InputStream is;
+    private ObjectMapper om;
+    private Workplace expected;
+
+    @Before
+    public void setUp() {
+	is = Thread.currentThread().getContextClassLoader().getResourceAsStream("workplace.xml");
+	om = new XmlMapper();
+	expected = new Workplace();
+	expected.setName("SWEDEN GREEN TRADING AB");
+	expected.setPostalCode("13140");
+	expected.setAdress("Kantatvägen 18");
+	expected.setCity("Nacka");
+	expected.setCountry("Sverige");
+	expected.setVisitingAddress("Kantatvägen 18");
+	expected.setVisitingCity("Nacka");
+	expected.setVisitingCountry("Sverige");
+	expected.setPhone("076-2899405");
+	expected.setEmail("11111@3333.3333");
+	expected.setHomepage("http://www.swedengreentrading.se");
+    }
+
+    @Test
+    public void testParseAd() throws JsonParseException, JsonMappingException, IOException {
+	Workplace actual = om.readValue(is, Workplace.class);
+	assertEquals(expected, actual);
+    }
+    
+    @After
+    public void tearDown() throws IOException {
+	is.close();
+    }
+}
