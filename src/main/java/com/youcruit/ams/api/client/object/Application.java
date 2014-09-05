889ff21d139f1de0e043294a58a6e79a444cd4c5
christianekstrand <christianekstrand@gmail.com>
2014-09-05 10:58:15 +0200

    Added support for additional fields in <ansokan>

--- src/main/java/com/youcruit/ams/api/client/object/Application.java
+++ src/main/java/com/youcruit/ams/api/client/object/Application.java
@@ -1,5 +1,7 @@
 package com.youcruit.ams.api.client.object;
 
+import java.util.Date;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
@@ -7,6 +9,12 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 public class Application {
     @JacksonXmlProperty(localName="epostadress")
     private String email;
+    @JacksonXmlProperty(localName="referens")
+    private String reference;
+    @JacksonXmlProperty(localName="sista_ansokningsdag")
+    private Date closeDate;
+    @JacksonXmlProperty(localName="ovrigt_om_ansokan")
+    private String additionalInformation;
 
     public String getEmail() {
         return email;
@@ -16,11 +24,38 @@ public class Application {
         this.email = email;
     }
 
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public Date getCloseDate() {
+        return closeDate;
+    }
+
+    public void setCloseDate(Date closeDate) {
+        this.closeDate = closeDate;
+    }
+
+    public String getAdditionalInformation() {
+        return additionalInformation;
+    }
+
+    public void setAdditionalInformation(String additionalInformation) {
+        this.additionalInformation = additionalInformation;
+    }
+
     @Override
     public int hashCode() {
 	final int prime = 31;
 	int result = 1;
+	result = prime * result + ((additionalInformation == null) ? 0 : additionalInformation.hashCode());
+	result = prime * result + ((closeDate == null) ? 0 : closeDate.hashCode());
 	result = prime * result + ((email == null) ? 0 : email.hashCode());
+	result = prime * result + ((reference == null) ? 0 : reference.hashCode());
 	return result;
     }
 
@@ -30,14 +65,23 @@ public class Application {
 	if (obj == null) return false;
 	if (!(obj instanceof Application)) return false;
 	Application other = (Application) obj;
+	if (additionalInformation == null) {
+	    if (other.additionalInformation != null) return false;
+	} else if (!additionalInformation.equals(other.additionalInformation)) return false;
+	if (closeDate == null) {
+	    if (other.closeDate != null) return false;
+	} else if (!closeDate.equals(other.closeDate)) return false;
 	if (email == null) {
 	    if (other.email != null) return false;
 	} else if (!email.equals(other.email)) return false;
+	if (reference == null) {
+	    if (other.reference != null) return false;
+	} else if (!reference.equals(other.reference)) return false;
 	return true;
     }
 
     @Override
     public String toString() {
-	return "Application [email=" + email + "]";
+	return "Application [email=" + email + ", reference=" + reference + ", closeDate=" + closeDate + ", additionalInformation=" + additionalInformation + "]";
     }
 }
---
+++ src/test/java/com/youcruit/ams/api/client/object/ApplicationTest.java
@@ -0,0 +1,52 @@
+package com.youcruit.ams.api.client.object;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class ApplicationTest {
+    private InputStream is;
+    private ObjectMapper om;
+    private Application expected;
+
+    @Before
+    public void setUp() {
+	is = Thread.currentThread().getContextClassLoader().getResourceAsStream("application.xml");
+	om = new XmlMapper();
+	expected = new Application();
+	expected.setEmail("kontakt@modelhouse.se");
+	expected.setReference("Säljare/värdinna");
+	expected.setAdditionalInformation("Fast lön");
+	Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Stockholm"));
+	cal.set(Calendar.YEAR, 2014);
+	cal.set(Calendar.MONTH, Calendar.SEPTEMBER);
+	cal.set(Calendar.DAY_OF_MONTH, 14);
+	cal.set(Calendar.HOUR_OF_DAY, 0);
+	cal.set(Calendar.MINUTE, 0);
+	cal.set(Calendar.SECOND, 0);
+	cal.set(Calendar.MILLISECOND, 0);
+	expected.setCloseDate(cal.getTime());
+    }
+
+    @Test
+    public void testParseDetails() throws IOException {
+	Application actual = om.readValue(is, Application.class);
+	assertEquals(expected, actual);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+	is.close();
+    }
+
+}
