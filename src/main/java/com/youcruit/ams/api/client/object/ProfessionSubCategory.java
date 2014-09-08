6e39291d0bd5b2015e99978cf582f2b377305eb4
christianekstrand <christianekstrand@gmail.com>
2014-09-08 14:22:44 +0200

    optimize imports

--- src/main/java/com/youcruit/ams/api/client/object/ProfessionSubCategory.java
+++ src/main/java/com/youcruit/ams/api/client/object/ProfessionSubCategory.java
@@ -1,7 +1,6 @@
 package com.youcruit.ams.api.client.object;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ProfessionSubCategory extends SearchItem {
