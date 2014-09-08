4db95a71320fe98e99e5e4ecb531ec7727c55845
christianekstrand <christianekstrand@gmail.com>
2014-09-08 13:48:27 +0200

    Added Profession

---
+++ src/main/java/com/youcruit/ams/api/client/object/Profession.java
@@ -0,0 +1,8 @@
+package com.youcruit.ams.api.client.object;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Profession extends SearchItem {
+
+}
---
+++ src/test/java/com/youcruit/ams/api/client/object/ProfessionTest.java
@@ -0,0 +1,41 @@
+package com.youcruit.ams.api.client.object;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class ProfessionTest {
+    private InputStream is;
+    private ObjectMapper om;
+    private Profession expected;
+
+    @Before
+    public void setUp() {
+	is = Thread.currentThread().getContextClassLoader().getResourceAsStream("profession.xml");
+	om = new XmlMapper();
+	expected = new Profession();
+	expected.setCount(0);
+	expected.setId("4742");
+	expected.setName("Bakteriolog");
+    }
+
+    @Test
+    public void testParseDetails() throws IOException {
+	Profession actual = om.readValue(is, Profession.class);
+	assertEquals(expected, actual);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+	is.close();
+    }
+
+}
