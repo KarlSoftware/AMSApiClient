8b44c901e4a6862ba96d361f14bcfc96eada64b7
Patrick Gilmore <p@noonday.se>
2014-09-09 13:48:12 +0200

    Added date created and upped version

--- src/main/java/com/youcruit/ams/api/client/AMSCache.java
+++ src/main/java/com/youcruit/ams/api/client/AMSCache.java
@@ -4,13 +4,20 @@ import com.youcruit.ams.api.client.object.Profession;
 import com.youcruit.ams.api.client.object.ProfessionCategory;
 import com.youcruit.ams.api.client.object.ProfessionSubCategory;
 
+import java.util.Date;
 import java.util.Map;
 
 public class AMSCache {
 
+    public enum CacheType {
+	LOOKUP
+    }
+
     private Map<String, Profession> profession;
     private Map<String, ProfessionCategory> professionCategories;
     private Map<String, ProfessionSubCategory> professionSubCategories;
+    private Date dateCreated;
+    private CacheType cacheType;
 
     public AMSCache() {}
 
@@ -18,6 +25,8 @@ public class AMSCache {
 	this.profession = profession;
 	this.professionCategories = professionCategories;
 	this.professionSubCategories = professionSubCategories;
+	this.dateCreated = new Date();
+	this.cacheType = CacheType.LOOKUP;
     }
 
     public Map<String, Profession> getProfession() {
@@ -42,5 +51,21 @@ public class AMSCache {
 
     public void setProfessionSubCategories(Map<String, ProfessionSubCategory> professionSubCategories) {
 	this.professionSubCategories = professionSubCategories;
+    }
+
+    public Date getDateCreated() {
+	return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+	this.dateCreated = dateCreated;
+    }
+
+    public CacheType getCacheType() {
+	return cacheType;
+    }
+
+    public void setCacheType(CacheType cacheType) {
+	this.cacheType = cacheType;
     }
 }
