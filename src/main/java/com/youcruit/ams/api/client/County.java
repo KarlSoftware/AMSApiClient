69b67804400b68776a55a302114ec103fbdcdfd5
christianekstrand <christianekstrand@gmail.com>
2014-09-11 15:06:14 +0200

    Added description to County

--- src/main/java/com/youcruit/ams/api/client/County.java
+++ src/main/java/com/youcruit/ams/api/client/County.java
@@ -1,35 +1,40 @@
 package com.youcruit.ams.api.client;
 
 public enum County {
-    BLEKINGE(10),
-    DALARNA(20),
-    GOTLAND(9),
-    GAVLEBORG(21),
-    HALLAND(13),
-    JAMTLAND(23),
-    JONKOPING(6),
-    KALMAR(8),
-    KRONOBERG(7),
-    NORRBOTTEN(25),
-    SKANE(12),
-    STOCKHOLM(1),
-    SODERMANLAND(4),
-    UPPSALA(3),
-    VARMLAND(17),
-    VASTERBOTTEN(24),
-    VASTERNORRLAND(22),
-    VASTMANLAND(19),
-    VASTRAGOTALAND(14),
-    OREBRO(18),
-    OSTERGOTLAND(5),
-    UNDEFINED_CITY(90);
+    BLEKINGE(10, "Blekinge"),
+    DALARNA(20, "Dalarna"),
+    GOTLAND(9, "Gotland"),
+    GAVLEBORG(21, "Gävleborg"),
+    HALLAND(13, "Halland"),
+    JAMTLAND(23, "Jämtland"),
+    JONKOPING(6, "Jönköping"),
+    KALMAR(8, "Kalmar"),
+    KRONOBERG(7, "Kronoberg"),
+    NORRBOTTEN(25, "Norrbotten"),
+    SKANE(12, "Skåne"),
+    STOCKHOLM(1, "Stockholm"),
+    SODERMANLAND(4, "Södermanland"),
+    UPPSALA(3, "Uppsala"),
+    VARMLAND(17, "Värmland"),
+    VASTERBOTTEN(24, "Västerbotten"),
+    VASTERNORRLAND(22, "Västernorrland"),
+    VASTMANLAND(19, "Västmanland"),
+    VASTRAGOTALAND(14, "Västra Götaland"),
+    OREBRO(18, "Örebro"),
+    OSTERGOTLAND(5, "Östergötland"),
+    UNDEFINED_CITY(90, "Ospecificerad arbetsort");
     
     private int id;
-    County(int id ){
+    private String description;
+    County(int id , String description){
 	this.id = id;
+	this.description = description;
     }
-    int getId(){
+    public int getId(){
 	return id;
+    }
+    public String getDescription() {
+	return description;
     }
     public static County fromId(int id){
 	for(County c : County.values()){
