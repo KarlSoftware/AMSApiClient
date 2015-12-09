9988218b5ea6e822f3954e2ad9bd70eed37a8686
Tobias Olsson <tobias.olsson@youcruit.com>
2015-12-09 12:39:37 +0100

    Dump data as json in INTtests

--- src/main/java/com/youcruit/ams/api/client/AMSSoapClient.java
+++ src/main/java/com/youcruit/ams/api/client/AMSSoapClient.java
@@ -1,12 +1,7 @@
 package com.youcruit.ams.api.client;
 
 import javax.xml.ws.BindingProvider;
-import javax.xml.ws.soap.SOAPBinding;
 
-import com.youcruit.ams.api.soap.Article;
-import com.youcruit.ams.api.soap.Occupation;
-import com.youcruit.ams.api.soap.OccupationList;
-import com.youcruit.ams.api.soap.OccupationlistShort;
 import com.youcruit.ams.api.soap.WSOccupation;
 import com.youcruit.ams.api.soap.WSOccupationSoap;
 
@@ -14,7 +9,7 @@ public class AMSSoapClient {
     public static final String ENDPOINT = "http://api.arbetsformedlingen.se/af/v0/Occupation/wsoccupation.asmx";
 
     public static WSOccupationSoap createClient() {
-	WSOccupationSoap wsOccupationSoap = new WSOccupation().getWSOccupationSoap()
+	WSOccupationSoap wsOccupationSoap = new WSOccupation().getWSOccupationSoap();
 	BindingProvider bindingProvider = (BindingProvider) wsOccupationSoap;
 	bindingProvider.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, ENDPOINT);
 	return wsOccupationSoap;
--- src/test/java/com/youcruit/ams/api/client/SoapINTTest.java
+++ src/test/java/com/youcruit/ams/api/client/SoapINTTest.java
@@ -2,44 +2,59 @@ package com.youcruit.ams.api.client;
 
 import java.net.MalformedURLException;
 
-import javax.xml.ws.BindingProvider;
-
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.youcruit.ams.api.soap.Article;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.youcruit.ams.api.soap.Occupation;
 import com.youcruit.ams.api.soap.OccupationList;
-import com.youcruit.ams.api.soap.WSOccupation;
+import com.youcruit.ams.api.soap.RelatedOccupation;
 import com.youcruit.ams.api.soap.WSOccupationSoap;
 
 public class SoapINTTest {
 
-    public static final String ENDPOINT = "http://api.arbetsformedlingen.se/af/v0/Occupation/wsoccupation.asmx";
     private WSOccupationSoap wsOccupationSoap;
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     @BeforeClass
     public static void setupDump() throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
-        Class.forName("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe").getField("dump").set(null, true);
+	//Class.forName("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe").getField("dump").set(null, true);
     }
 
     @Before
     public void setup() throws MalformedURLException {
-        wsOccupationSoap = new WSOccupation().getWSOccupationSoap();
-        BindingProvider bindingProvider = (BindingProvider) this.wsOccupationSoap;
-        bindingProvider.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, ENDPOINT);
+	wsOccupationSoap = AMSSoapClient.createClient();
     }
 
     @Test
     public void getOccupations() {
-        OccupationList result = wsOccupationSoap.getAllOccupationsDetailed();
-        System.err.println(result);
+	OccupationList allOccupations = wsOccupationSoap.getAllOccupationsDetailed();
+	//wsOccupationSoap.getAllOccupations().getOccupation().get(0).getArticles().getArticleLink().get(0);
+
+	Occupation occupationToCheck = allOccupations.getOccupation().get(0);
+	//System.err.println(wsOccupationSoap.loadAllOccupationIdAndName());
+	//System.err.println(wsOccupationSoap.loadAllEducationConnections());
+	for (RelatedOccupation relatedOccupation : occupationToCheck.getRelatedOccupations().getRelatedOccupation()) {
+	    Occupation occupationById = wsOccupationSoap.getOccupationById(relatedOccupation.getId().intValue());
+	    System.out.println(occupationById);
+	}
+
+	String localeGroups = wsOccupationSoap.getLocaleGroups(696);
+	System.out.println(gson.toJson(allOccupations));
     }
 
+    @Test
+    public void getOccupation() {
+	Occupation occupationById = wsOccupationSoap.getOccupationById(696);
+	System.err.println(gson.toJson(occupationById));
+    }
 
     @Test
     public void getAds() {
-        Article result = wsOccupationSoap.getArticle(2817199);
-            System.err.println(result);
+	Article result = wsOccupationSoap.getArticle(2817199);
+	System.err.println(result);
     }
 }
