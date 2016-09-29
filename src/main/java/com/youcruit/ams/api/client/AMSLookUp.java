7cbfe101c7ac50a361e13d6449850811cddc3e09
christianekstrand <christianekstrand@gmail.com>
2016-09-29 12:19:32 +0200

    forceFetch should force a fetch

--- src/main/java/com/youcruit/ams/api/client/AMSLookUp.java
+++ src/main/java/com/youcruit/ams/api/client/AMSLookUp.java
@@ -42,23 +42,21 @@ public class AMSLookUp {
     }
 
     public void forceFetch(String amsBaseUrl, String fromEmail) throws IOException, URISyntaxException {
-	if (professionCache == null) {
-	    Cache cache = new Cache();
-	    AMSQuery pCQuery = new AMSQueryBuilder(AMSQuery.EndPoint.PROFESSION_CATEGORIES).build();
-	    ProfessionCategoryList pCList = new AMSApiClient(amsBaseUrl, fromEmail).executeQuery(pCQuery, ProfessionCategoryList.class);
-	    for (ProfessionCategory pc : pCList.getList()) {
-		List<ProfessionSubCategory> subCategories = fetchSubCategories(Integer.parseInt(pc.getAmsId()), amsBaseUrl, fromEmail);
-		for (ProfessionSubCategory psc : subCategories) {
-		    List<Profession> pL = fetchProfessions(Integer.parseInt(psc.getAmsId()), amsBaseUrl, fromEmail);
-		    for (Profession p : pL) {
-			cache.profession.put(p.getAmsId(), p);
-			cache.professionSubCategories.put(p.getAmsId(), psc);
-		    }
-		    cache.professionCategories.put(psc.getAmsId(), pc);
+	Cache cache = new Cache();
+	AMSQuery pCQuery = new AMSQueryBuilder(AMSQuery.EndPoint.PROFESSION_CATEGORIES).build();
+	ProfessionCategoryList pCList = new AMSApiClient(amsBaseUrl, fromEmail).executeQuery(pCQuery, ProfessionCategoryList.class);
+	for (ProfessionCategory pc : pCList.getList()) {
+	    List<ProfessionSubCategory> subCategories = fetchSubCategories(Integer.parseInt(pc.getAmsId()), amsBaseUrl, fromEmail);
+	    for (ProfessionSubCategory psc : subCategories) {
+		List<Profession> pL = fetchProfessions(Integer.parseInt(psc.getAmsId()), amsBaseUrl, fromEmail);
+		for (Profession p : pL) {
+		    cache.profession.put(p.getAmsId(), p);
+		    cache.professionSubCategories.put(p.getAmsId(), psc);
 		}
+		cache.professionCategories.put(psc.getAmsId(), pc);
 	    }
-	    this.professionCache = cache;
 	}
+	this.professionCache = cache;
     }
 
     @Deprecated
